f222ccd09846d8352f6c7813bbe30324fb7ab8c6
Bikash-92 <110325682+Bikash-92@users.noreply.github.com>
2025-01-28 00:37:29 +0530

    commented testcases

--- paymentservice/src/test/java/com/ekart/PaymentserviceApplicationTests.java
+++ paymentservice/src/test/java/com/ekart/PaymentserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ekart;
+/*package com.ekart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class PaymentserviceApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
