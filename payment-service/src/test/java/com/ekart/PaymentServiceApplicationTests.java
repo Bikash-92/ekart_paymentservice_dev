985800153cf2b741c07507f2311c52aa850d5d68
Bikash-92 <110325682+Bikash-92@users.noreply.github.com>
2025-02-03 12:41:28 +0530

    Update PaymentServiceApplicationTests.java

--- payment-service/src/test/java/com/ekart/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/ekart/PaymentServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ekart;
+/*package com.ekart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class PaymentServiceApplicationTests {
 	}
 
 }
+*/
